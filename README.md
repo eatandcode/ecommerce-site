212b3a06446c1a4d6dbb8d0f1d0e15f9369af2d7
nickcancode <nickhidalgo93@gmail.com>
2020-12-06 09:14:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-un4u6k
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-un4u6k)
---
+++ src/app/menu-overview-example.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic menu
+ */
+@Component({
+  selector: 'menu-overview-example',
+  templateUrl: 'menu-overview-example.html',
+})
+export class MenuOverviewExample {}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
